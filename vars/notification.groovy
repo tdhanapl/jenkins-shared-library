0a4b3ced3fd612d526fc8c6c79076714d1f0c019
dhanapal703278 <61935257+dhanapal703278@users.noreply.github.com>
2022-06-27 18:26:26 +0530

    Update notification.groovy

--- vars/notification.groovy
+++ vars/notification.groovy
@@ -1,18 +1,18 @@
-def call(string buildstatus= 'STARTED') {
+def call(String buildStatus= 'STARTED') {
   //build status of null means successfull 
   buildStatus = buildStatus ?: 'SUCCESS'
   //default values 
-  def colorname = 'RED'
+  def colorName = 'RED'
   def colorCode = '#FF000'
-  def subject = "${ buildStatus } : Job '${ env.JOB_nAME} [${env.BUILD_NUMBER}]"
-  def summary = "${ subject} (${env.BUILD_URL}"
-  def details = """<p>${buildStatus}; Job ${env.JOB_NAME} [$ {env.BUIL_NUMBER}':</p>
-  <p>Check console output at & QUOT;<a hrof=${env.BULD_URL}'>${env.JOB_NAME} [$env.BUILD_NUMBER}]</a>"""
+  def subject = "${ buildStatus }: Job '${ env.JOB_nAME} [${env.BUILD_NUMBER}]'"
+  def summary = "${ subject} (${env.BUILD_URL})"
+  def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [$ {env.BUIL_NUMBER}':</p>
+  <p>Check console output at & QUOT;<a hrof=${env.BULD_URL}'>${env.JOB_NAME} [$env.BUILD_NUMBER}] </a>&QUOT</p>"""
   //override default values based on buld status
-  if(buldstatus == 'STARTED') {
+  if (buldStatus == 'STARTED') {
     color = 'YELLOW'
     colorCode = '#FF000'
-  }else if(buldstatus == 'SUCCESS') {
+  } else if (buldstatus == 'SUCCESS') {
     color = 'GREEN'
     colorCode = '#00FF00
   } else {
@@ -25,6 +25,8 @@ def call(string buildstatus= 'STARTED') {
     to: 'dhanapal703278@gmail.com',
     subject: subject,
     body: details,
-    recipientProviders: [[$class; 'DeveloperRecipientProvider']]
+    recipientProviders: [[$class: 'DeveloperRecipientProvider']]
+    )
+}
   
   
