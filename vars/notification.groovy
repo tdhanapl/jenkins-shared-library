0e48d034ac09bccfaec6cae7548dfe5ec46420b1
dhanapal703278 <61935257+dhanapal703278@users.noreply.github.com>
2022-06-27 18:09:26 +0530

    Create notification.groovy

---
+++ vars/notification.groovy
@@ -0,0 +1,30 @@
+def call(string buildstatus= 'STARTED') {
+  //build status of null means successfull 
+  buildStatus = buildStatus ?: 'SUCCESS'
+  //default values 
+  def colorname = 'RED'
+  def colorCode = '#FF000'
+  def subject = "${ buildStatus } : Job '${ env.JOB_nAME} [${env.BUILD_NUMBER}]"
+  def summary = "${ subject} (${env.BUILD_URL}"
+  def details = """<p>${buildStatus}; Job ${env.JOB_NAME} [$ {env.BUIL_NUMBER}':</p>
+  <p>Check console output at & QUOT;<a hrof=${env.BULD_URL}'>${env.JOB_NAME} [$env.BUILD_NUMBER}]</a>"""
+  //override default values based on buld status
+  if(buldstatus == 'STARTED') {
+    color = 'YELLOW'
+    colorCode = '#FF000'
+  }else if(buldstatus == 'SUCCESS') {
+    color = 'GREEN'
+    colorCode = '#00FF00
+  } else {
+    color = 'RED'
+    colorCode = '#FF0000'
+  }
+  //send notification
+  //slack [color: colorcode, notify; true, message: summary)
+  emailtext(
+    to: 'dhanapal703278@gmail.com',
+    subject: subject,
+    body: details,
+    recipientProviders: [[$class; 'DeveloperRecipientProvider']]
+  
+  
